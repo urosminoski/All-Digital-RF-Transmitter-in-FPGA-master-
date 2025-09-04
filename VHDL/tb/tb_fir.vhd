69838cd28a39d90beac76cf06db19838ac602247
urosminoski <uros.minoski@pupin.rs>
2025-09-04 20:31:12 +0200

    ..

--- VHDL/tb/tb_fir.vhd
+++ VHDL/tb/tb_fir.vhd
@@ -20,15 +20,16 @@ architecture tb of tb_fir is
 	signal clk0, clk1, clk2   	: std_logic := '0';
 	signal rst       			: std_logic := '1';
 	signal x         			: std_logic_vector(WIDTH_X-1 downto 0) := (others => '0');
-	signal y         			: std_logic_vector(WIDTH_Y-1 downto 0) := (others => '0');
+	signal y0, y1         		: std_logic_vector(WIDTH_Y-1 downto 0) := (others => '0');
 	-- signal x : sfixed(3 downto -8);
 	-- signal y : sfixed(3 downto 0);
 
 	-- Lokalni “handshake” u TB:
 	signal out_ready : std_logic := '0';
 
-	file input_file  	: text open read_mode  is "C:\Users\Korisnik\Desktop\FAKS\MASTER\All-Digital-RF-Transmitter-in-FPGA-master-\VHDL\data\interpolation_test\xin_test.txt";
-	file output_file  	: text open write_mode  is "C:\Users\Korisnik\Desktop\FAKS\MASTER\All-Digital-RF-Transmitter-in-FPGA-master-\VHDL\data\interpolation_test\xout_test.txt";
+	file input_file  		: text open read_mode  is "C:\Users\Korisnik\Desktop\FAKS\MASTER\All-Digital-RF-Transmitter-in-FPGA-master-\VHDL\data\interpolation_test\xin_test.txt";
+	file output_file_0  	: text open write_mode  is "C:\Users\Korisnik\Desktop\FAKS\MASTER\All-Digital-RF-Transmitter-in-FPGA-master-\VHDL\data\interpolation_test\xout0_test.txt";
+	file output_file_1  	: text open write_mode  is "C:\Users\Korisnik\Desktop\FAKS\MASTER\All-Digital-RF-Transmitter-in-FPGA-master-\VHDL\data\interpolation_test\xout1_test.txt";
 
 begin
 	uut: entity work.fir0
@@ -42,7 +43,8 @@ begin
 			clk2 => clk2,
 			rst => rst,
 			x   => x,
-			y   => y
+			y0   => y0,
+			y1   => y1
 		);
 
 	clk0 <= not clk0 after C_CLK_PERIOD/2;
@@ -84,14 +86,17 @@ begin
 	end process;
 
 
-	write_files : process(clk1)
-		variable L : line;
+	write_files : process(clk0)
+		variable L_0, L_1 : line;
 	begin
-		if falling_edge(clk1) then
+		if falling_edge(clk0) then
 			if out_ready = '1' then
 				-- upis I izlaza
-				write(L, to_integer(signed(y)));
-				writeline(output_file, L);
+				write(L_1, to_integer(signed(y0)));
+				writeline(output_file_0, L_1);
+				
+				write(L_0, to_integer(signed(y1)));
+				writeline(output_file_1, L_0);
 			end if;
 		end if;
 	end process;
