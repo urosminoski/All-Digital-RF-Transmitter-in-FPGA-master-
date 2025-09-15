930938854d19538d77ce224d3e4ebae3ca33f2ea
urosminoski <uros.minoski@pupin.rs>
2025-09-15 20:55:04 +0200

    Delay test

--- VHDL/src/stage1.vhd
+++ VHDL/src/stage1.vhd
@@ -32,13 +32,16 @@ architecture rtl of stage1 is
 
 	constant LUT_ID : integer := 3;
 	
-	constant DELTA_I : real := -0.125;
-	constant DELTA_Q : real := 0.125;
+	constant DELTA 		: real := 0.00390625;
+	constant DELTA_I 	: real := -DELTA;
+	constant DELTA_Q 	: real := DELTA;
 
-	signal ds_factor	: sfixed(4 downto -(XWIDTH-5));
-	signal xi, xq 		: sfixed(3 downto -(XWIDTH-4));
+	signal factor1, factor2	: sfixed(4 downto -(XWIDTH-5));
+	signal xi_1, xq_1 		: sfixed(3 downto -(XWIDTH-4));
+	signal xi_2, xq_2 		: sfixed(3 downto -(XWIDTH-4));
 	
-	signal xout_i_delay, xout_q_delay : std_logic_vector(XWIDTH-1 downto 0);
+	signal xin_i_delay, xin_q_delay 	: std_logic_vector(XWIDTH-1 downto 0);
+	signal xout_i_delay, xout_q_delay 	: std_logic_vector(XWIDTH-1 downto 0);
 	
 	signal xout_i_osr8, xout_q_osr8 : std_logic_vector(XWIDTH-1 downto 0);
 	signal vout_i, vout_q 	: std_logic := '0';
@@ -104,7 +107,8 @@ begin
 			-- xout	=> xout_q_delay      
 		-- );
 
-	ds_factor <= to_sfixed(30, ds_factor'high, ds_factor'low);
+	factor1 <= to_sfixed(7.5, factor1'high, factor1'low);
+	factor2 <= to_sfixed(4, factor2'high, factor2'low);
 
 	osr8_i: entity work.osr8
 		generic map (
@@ -138,14 +142,56 @@ begin
 			vout 	=> vout_q
 		);
 		
-	xout_i_osr8_test	<= xout_i_osr8;
-	xout_q_osr8_test	<= xout_q_osr8;
+	-- xout_i_osr8_test	<= xout_i_osr8;
+	-- xout_q_osr8_test	<= xout_q_osr8;
 		
-	xi <= resize(to_sfixed(xout_i_osr8, 0, -(XWIDTH-1)) * ds_factor, xi'high, xi'low);
-	xq <= resize(to_sfixed(xout_q_osr8, 0, -(XWIDTH-1)) * ds_factor, xq'high, xq'low);
+	xi_1 <= resize(to_sfixed(xout_i_osr8, 0, -(XWIDTH-1)) * factor1, xi_1'high, xi_1'low);
+	xq_1 <= resize(to_sfixed(xout_q_osr8, 0, -(XWIDTH-1)) * factor1, xq_1'high, xq_1'low);
 	
-	xin_i_ds <= to_slv(xi);
-	xin_q_ds <= to_slv(xq);
+	xin_i_delay <= to_slv(xi_1);
+	xin_q_delay <= to_slv(xq_1);
+	
+	delay_i: entity work.delay
+		generic map (
+			KERNEL_ID   => 7,
+			COEF_L		=> COEF_L,
+			XWIDTH		=> XWIDTH,
+			INT  		=> INT,
+			FRAC 		=> FRAC,
+			NUM_TAPS   	=> 7,
+			DELTA		=> DELTA_I
+		)
+		port map (
+			clk		=> clk,
+			rst		=> rst, 
+			en		=> '1', 		
+			xin		=> xin_i_delay,
+			xout	=> xout_i_delay      
+		);
+		
+	delay_q: entity work.delay
+		generic map (
+			KERNEL_ID   => 7,
+			COEF_L		=> COEF_L,
+			XWIDTH		=> XWIDTH,
+			INT  		=> INT,
+			FRAC 		=> FRAC,
+			NUM_TAPS   	=> 7,
+			DELTA		=> DELTA_Q
+		)
+		port map (
+			clk		=> clk,
+			rst		=> rst, 
+			en		=> '1', 		
+			xin		=> xin_q_delay,
+			xout	=> xout_q_delay      
+		);
+		
+	xi_2 <= resize(to_sfixed(xout_i_delay, 0, -(XWIDTH-1)) * factor2, xi_2'high, xi_2'low);
+	xq_2 <= resize(to_sfixed(xout_q_delay, 0, -(XWIDTH-1)) * factor2, xq_2'high, xq_2'low);
+	
+	xin_i_ds <= to_slv(xi_2);
+	xin_q_ds <= to_slv(xq_2);
 	
 	deltaSigma_i: entity work.deltaSigma
 		port map (
