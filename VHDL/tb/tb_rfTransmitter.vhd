dbaf25630d59da1512c69b81b7ec429fa2495af7
urosminoski <uros.minoski@pupin.rs>
2025-09-18 18:06:50 +0200

    Results for transmitter.

--- VHDL/tb/tb_rfTransmitter.vhd
+++ VHDL/tb/tb_rfTransmitter.vhd
@@ -19,8 +19,8 @@ architecture tb of tb_rfTransmitter is
 	
 	constant LUT_ID			: integer := 3;
 	constant KERNEL_ID		: integer := 7;
-	constant DS_WIDTH		: integer := 12;
-	constant OSR_WIDTH		: integer := 16;
+	constant DS_WIDTH		: integer := 24;
+	constant OSR_WIDTH		: integer := 24;
 	constant OSR_COEFF		: integer := 16;
 	constant OSR_GUARD_BITS	: integer := 4;
 
