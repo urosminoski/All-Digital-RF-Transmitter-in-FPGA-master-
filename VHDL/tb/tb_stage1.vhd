edc8e3ec15246a5152657bd05302a80968b31c27
urosminoski <uros.minoski@pupin.rs>
2025-09-18 15:43:07 +0200

    Primary is done. Now secondary is allmost cmplete.

--- VHDL/tb/tb_stage1.vhd
+++ VHDL/tb/tb_stage1.vhd
@@ -15,8 +15,8 @@ architecture tb of tb_stage1 is
 	constant C_CLK0_PERIOD 	: time    := 10240 ps;
 	constant C_CLK1_PERIOD 	: time    := 1280 ps;
 	
-	constant DS_WIDTH		: integer := 12;
-	constant OSR_WIDTH		: integer := 12;
+	constant DS_WIDTH		: integer := 24;
+	constant OSR_WIDTH		: integer := 24;
 	constant OSR_COEFF		: integer := 15;
 	constant OSR_GUARD_BITS : integer := 4;
 
