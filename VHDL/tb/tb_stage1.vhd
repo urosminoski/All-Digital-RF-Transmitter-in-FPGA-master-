d0a6bfd007be91d14a3067cad2d3aa8cc3ddadf8
urosminoski <uros.minoski@pupin.rs>
2025-09-18 17:34:45 +0200

    Results for transmitter.

--- VHDL/tb/tb_stage1.vhd
+++ VHDL/tb/tb_stage1.vhd
@@ -15,8 +15,8 @@ architecture tb of tb_stage1 is
 	constant C_CLK0_PERIOD 	: time    := 10240 ps;
 	constant C_CLK1_PERIOD 	: time    := 1280 ps;
 	
-	constant DS_WIDTH		: integer := 16;
-	constant OSR_WIDTH		: integer := 16;
+	constant DS_WIDTH		: integer := 12;
+	constant OSR_WIDTH		: integer := 12;
 	constant OSR_COEFF		: integer := 17;
 	constant OSR_GUARD_BITS : integer := 4;
 
